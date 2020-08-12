8f4c7d240df6a4d28089e90b9629fcd879433a5e
caohieu1509 <caovanhieuhaui@.com>
2020-08-12 11:41:34 +0700

    update abc caohieu

--- app/src/main/java/com/example/extodo2/editTask/EditTaskFragment.java
+++ app/src/main/java/com/example/extodo2/editTask/EditTaskFragment.java
@@ -22,20 +22,14 @@ public class EditTaskFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public EditTaskFragment() {
         // Required empty public constructor
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditTaskFragment.
+
      */
     // TODO: Rename and change types and number of parameters
     public static EditTaskFragment newInstance(String param1, String param2) {
@@ -51,8 +45,8 @@ public class EditTaskFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
