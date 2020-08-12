37968c80ac3287dc40731b3d90682b0091f07b57
quyen <48308419+thaibaquyen@users.noreply.github.com>
2020-08-12 11:26:42 +0700

    update on thach

--- app/src/main/java/com/example/extodo2/data/TaskRespository.java
+++ app/src/main/java/com/example/extodo2/data/TaskRespository.java
@@ -46,6 +46,7 @@ public class TaskRespository implements TaskDataSource {
         mTasksLocalDataSource.saveTask(task);
     }
 
+    // thach dai gia
     @Override
     public void completeTask(Task task) {
         mTasksLocalDataSource.completeTask(task);
